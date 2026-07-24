ee504fc15bb6f9eeed6dfc15cce1a23485e8978b
Jorge Castillo <schorts@outlook.com>
2026-07-23 20:02:51 -0600

    refactor: Convert `DAO` from `abstract class` to `interface`.

--- src/dao/DAO.php
+++ src/dao/DAO.php
@@ -7,26 +7,20 @@ use Schorts\SharedKernel\Criteria\Criteria;
 use Schorts\SharedKernel\UnitOfWork\UnitOfWork;
 use Schorts\SharedKernel\DAO\DeleteMode;
 
-abstract class DAO
+interface DAO
 {
-  protected DeleteMode $deleteMode;
-
-  public function __construct(DeleteMode $deleteMode = DeleteMode::HARD)
-  {
-    $this->deleteMode = $deleteMode;
-  }
-
-  abstract public function getAll(?UnitOfWork $uow = null, bool $includeDeleted = false): array;
-  abstract public function findByID(mixed $id, ?UnitOfWork $uow = null, bool $includeDeleted = false): ?Entity;
-  abstract public function findOneBy(Criteria $criteria, ?UnitOfWork $uow = null, bool $includeDeleted = false): ?Entity;
-  abstract public function search(Criteria $criteria, ?UnitOfWork $uow = null, bool $includeDeleted = false): array;
-  abstract public function countBy(Criteria $criteria, ?UnitOfWork $uow = null, bool $includeDeleted = false): int;
-  abstract public function exists(Criteria $criteria, ?UnitOfWork $uow = null, bool $includeDeleted = false): bool;
-  abstract public function create(Entity $entity, ?UnitOfWork $uow = null): Entity;
-  abstract public function update(Entity $entity, ?UnitOfWork $uow = null): Entity;
-  abstract public function save(Entity $entity, ?UnitOfWork $uow = null): Entity;
-  abstract public function delete(Entity $entity, ?UnitOfWork $uow = null): Entity;
-  abstract public function deleteByID(mixed $id, ?UnitOfWork $uow = null): void;
-  abstract public function saveMany(array $entities, ?UnitOfWork $uow = null): array;
-  abstract public function restore(Entity $entity, ?UnitOfWork $uow = null): Entity;
+  public function getAll(?UnitOfWork $uow = null, bool $includeDeleted = false): array;
+  public function findByID(mixed $id, ?UnitOfWork $uow = null, bool $includeDeleted = false): ?Entity;
+  public function findOneBy(Criteria $criteria, ?UnitOfWork $uow = null, bool $includeDeleted = false): ?Entity;
+  public function search(Criteria $criteria, ?UnitOfWork $uow = null, bool $includeDeleted = false): array;
+  public function count(?UnitOfWork $uow = null, bool $includeDeleted = false): int;
+  public function countBy(Criteria $criteria, ?UnitOfWork $uow = null, bool $includeDeleted = false): int;
+  public function exists(Criteria $criteria, ?UnitOfWork $uow = null, bool $includeDeleted = false): bool;
+  public function create(Entity $entity, ?UnitOfWork $uow = null): Entity;
+  public function update(Entity $entity, ?UnitOfWork $uow = null): Entity;
+  public function save(Entity $entity, ?UnitOfWork $uow = null): Entity;
+  public function delete(Entity $entity, ?UnitOfWork $uow = null): Entity;
+  public function deleteByID(mixed $id, ?UnitOfWork $uow = null): void;
+  public function saveMany(array $entities, ?UnitOfWork $uow = null): array;
+  public function restore(Entity $entity, ?UnitOfWork $uow = null): Entity;
 }
