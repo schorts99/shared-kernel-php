024401c8a1ad47e6be52d614d8e161dfc5768d51
Jorge Castillo <schorts@outlook.com>
2026-07-31 20:24:17 -0600

    feat: Add `toArray` to `QueryPrimitives`.

--- src/CQRS/Query/QueryPrimitives.php
+++ src/CQRS/Query/QueryPrimitives.php
@@ -18,4 +18,22 @@ final readonly class QueryPrimitives
     public ?array $headers,
     public ?array $context,
   ) {}
+
+  public function toArray(): array
+  {
+    return [
+      'id'             => $this->id,
+      'type'           => $this->type,
+      'created_at'     => $this->created_at,
+      'correlation_id' => $this->correlation_id,
+      'causation_id'   => $this->causation_id,
+      'request_id'     => $this->request_id,
+      'version'        => $this->version,
+      'user_id'        => $this->user_id,
+      'tenant_id'      => $this->tenant_id,
+      'payload'        => $this->payload,
+      'headers'        => $this->headers,
+      'context'        => $this->context,
+    ];
+  }
 }
