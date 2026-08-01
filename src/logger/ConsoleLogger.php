af5840569631179437ad41a42e414103d2ba2426
Jorge Castillo <schorts@outlook.com>
2026-08-01 00:45:07 -0600

    refactor: Move `LogLevel` to a separate file.

--- src/logger/ConsoleLogger.php
+++ src/logger/ConsoleLogger.php
@@ -2,24 +2,6 @@
 
 namespace Schorts\SharedKernel\Logger;
 
-enum LogLevel: string {
-  case DEBUG = 'debug';
-  case INFO  = 'info';
-  case LOG   = 'log';
-  case WARN  = 'warn';
-  case ERROR = 'error';
-
-  public function valueRank(): int {
-    return match($this) {
-      self::DEBUG => 0,
-      self::INFO  => 1,
-      self::LOG   => 2,
-      self::WARN  => 3,
-      self::ERROR => 4,
-    };
-  }
-}
-
 class ConsoleLogger extends Logger
 {
   private int $levelValue;
